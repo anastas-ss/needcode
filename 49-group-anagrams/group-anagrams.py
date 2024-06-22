67ef4650d400bba4e6069e6411f324a731c7bf05
anastas-ss <144778248+anastas-ss@users.noreply.github.com>
2024-06-22 22:42:02 +0300

    Time: 91 ms (51.56%) | Memory: 19.7 MB (74.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,17 +2,6 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        def isAnagram(self, s: str, t: str) -> bool:
-            if len(s) != len(t):
-                return False
-            a = list(s)
-            for i in range(0, len(t)):
-                if t[i] in a:
-                    a.remove(t[i])
-                else:
-                    return False
-            return True
-
         box = defaultdict(list)
         
         for word in strs:
